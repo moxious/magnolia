6c73cb710016082d8a74130c882061d5b7227716
Moxious <moxious@oldhat.org>
2018-08-11 15:13:17 -0400

    tweak

--- src/main/java/com/neo4j/magnolia/polyglot/CallExternal.java
+++ src/main/java/com/neo4j/magnolia/polyglot/CallExternal.java
@@ -12,20 +12,14 @@ import org.neo4j.procedure.UserFunction;
 import java.io.IOException;
 
 public class CallExternal {
-    static {
-        try {
-            MagnoliaConfiguration.initialize();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     @Context public GraphDatabaseService db;
     @Context public Log log;
 
-    @UserFunction("magnolia.call")
-    @Description("RETURN magnolia.call('scriptname', { map: value }) | call a magnolia script with a given argument set")
-    public Object call(final @Name("functionName") String externalFn, final @Name("arguments") Object arguments) throws IOException {
+    @UserFunction("magnolia.dynamic")
+    @Description("RETURN magnolia.dynamic('scriptname', { map: value }) | call a magnolia script with a given argument set")
+    public Object dynamic(final @Name("functionName") String externalFn, final @Name("arguments") Object arguments) throws IOException {
+        MagnoliaConfiguration.initialize();
+
         // Look up function, instantiate, run, and return its results.
         ExternalFnConfig config = MagnoliaConfiguration.config.getFunctionByName(externalFn);
         ExternalFn fn = new ExternalFn(config);
